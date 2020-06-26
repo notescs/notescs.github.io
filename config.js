6f49f1166f61f6a3e88c70af6994be61d93f1258
ayan-b <ayanbanerjee7777@gmail.com>
2020-06-26 09:53:46 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://notescs.gitlab.io',
+  pathPrefix: '/',
+  title: '',
+  subtitle: '',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-120952687-1',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'John Doe',
+    photo: '/photo.jpg',
+    bio: '',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
---
+++ content/posts/custom-compare-cpp-priority-queue.md
@@ -0,0 +1,15 @@
+---
+title: Using Custom Compare Function in C++ Priority Queue
+date: "2020-06-26"
+template: "post"
+draft: false
+slug: "custom-compare-priority-queue-cpp"
+category: "cpp"
+tags:
+  - "cpp"
+  - "priority queue"
+description: ""
+---
+
+
+
